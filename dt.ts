e26082821f9f68f265cb0735a45848a680764ddb
bryan2000 <bryanhard@gmail.com>
2018-10-25 11:08:51 -0400

    interface structure

---
+++ dt.ts
@@ -0,0 +1,63 @@
+
+export interface styleCode{
+    name: string;
+    img:string;
+    }
+  
+   
+  
+   export interface stockItem{
+    id:string;
+    code:string;
+    stylecode:string; //LO
+    descr:string;
+    itemtype:string;
+    rqside:boolean;
+    rqkd:boolean;
+    stock:string; // low InStock out
+    price:number;
+    price2:number;
+    price3:number;
+    cost:number;
+     }
+
+
+     export interface quotItem{
+    quotid: string;
+    quotitemid:string;
+    stockitemid:string;
+    code:string; // W1530
+    stylecode:string; //LO
+    itemtype: string //cab,acc,others,
+    descr:string;
+    rqside:boolean;
+    lside:boolean;
+    side:string;// left Right
+    rqkd:boolean;
+    kd:boolean; // yes=KD; no=Assemble
+    qty: number;
+    price:number; //assembled price
+    price2:number; // kd price
+    price3:number;// special price
+    discount:number; // applied discount amount of the item from vendor setting,;
+    cost:number;
+     stock:string; // low InStock out
+    }
+  
+    export interface quotItemSummary{
+      cabinet:number; //Piece
+      accessary:number; // piece
+      others:number; // piece
+      totalpiece:number;//piece
+      
+      discount:number;
+      subtotal:number;
+      tax:number;
+  
+      deliveryfee:number;
+      total:number;
+      styles:number;
+  
+    }
+ 
+ 
